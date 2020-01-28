7b2585fa7eb27b7d2b88fd0c7a88e10571274cc2
Zak <rusty_clarinet@yahoo.com>
2020-01-27 21:57:22 -0500

    outbrake first commit

---
+++ exercises/breakout/Assets/BlockDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDestroy : MonoBehaviour
+{
+    
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+        //instantiate(particle, transform~~~
+        //blockLeft = -1;
+
+    }
+}
