eeaffa5516d87fb27f4f4610260984fe8e2408f2
Zak <rusty_clarinet@yahoo.com>
2020-02-03 00:25:35 -0500

    1st commit

---
+++ assignment/AngryPigs/Assets/PaddleMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public float paddleSpeed = 1f;
+    public float leftEdge = -2.43f;
+    public float rightEdge = 2.43f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveLeft) && transform.position.x > leftEdge)
+        {
+            transform.position = new Vector3(transform.position.x - paddleSpeed, transform.position.y, 0);
+        }
+        if (Input.GetKey(moveRight) && transform.position.x < rightEdge)
+        {
+            transform.position = new Vector3(transform.position.x + paddleSpeed, transform.position.y, 0);
+        }
+    }
+}
+
+
+
+
+        //if (Input.GetKey(upKey) && transform.position.y< 3.5f)
+      //  {
+       //     transform.position = new Vector3(transform.position.x, transform.position.y + paddleSpeed, 0);
