1b205f33ec36adb39e65cf92eda743eab4e7f5dc
Zak <rusty_clarinet@yahoo.com>
2020-02-18 16:32:12 -0500

    W5 Commit 1

---
+++ assignment/RandomChess/Assets/_script/IntroAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAnimation : MonoBehaviour
+{
+    public Animator cameraAnimator;
+    public KeyCode StarterW;
+    public KeyCode StarterB;
+    const string WhitePlay = "WhitePlay";
+    const string BlackPlay = "BlackPlay";
+
+    void Start()
+    {
+        //cameraAnimator.GetComponent<cameraAnimator>(); -> dont need this because we have public Animator
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(StarterW))
+        {
+            cameraAnimator.SetTrigger(WhitePlay);
+        }
+        if (Input.GetKeyDown(StarterB))
+        {
+            cameraAnimator.SetTrigger(BlackPlay);
+        }
+    }
+}
