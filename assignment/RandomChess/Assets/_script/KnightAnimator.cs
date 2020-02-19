8ba9b9c9f99e15e65471eb369f6e943986884aea
Zak <rusty_clarinet@yahoo.com>
2020-02-19 14:03:46 -0500

    W5 commit 7

---
+++ assignment/RandomChess/Assets/_script/KnightAnimator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightAnimator : MonoBehaviour
+{
+    public Animator knightAnimator;
+    public KeyCode walk;
+    public KeyCode turn90L;
+    public KeyCode turn90R;
+    public KeyCode turn45L;
+    public KeyCode turn45R;
+    public KeyCode attack;
+    public KeyCode die;
+    public KeyCode idle;
+    const string moveStraight = "Straight";
+    const string turnL = "turnL";
+    const string turnR = "turnR";
+    const string sideL = "sideL";
+    const string sideR = "sideR";
+    const string hit = "Attack";
+    const string attacked = "Die";
+    const string idling = "idle";
+
+    void Start()
+    {
+        //cameraAnimator.GetComponent<cameraAnimator>(); -> dont need this because we have public Animator
+    }
+
+    void Update()
+    {
+        //ALL THE TRIGGERS SHOULD BE UPDATED LATER, THESE KEY INPUT IS ONLY FOR TESTING
+        if (Input.GetKeyDown(walk))
+        {
+            knightAnimator.SetTrigger(moveStraight);
+        }
+        if (Input.GetKeyDown(turn90L))
+        {
+            knightAnimator.SetTrigger(turnL);
+        }
+        if (Input.GetKeyDown(turn90R))
+        {
+            knightAnimator.SetTrigger(turnR);
+        }
+        if (Input.GetKeyDown(turn45L))
+        {
+            knightAnimator.SetTrigger(sideL);
+        }
+        if (Input.GetKeyDown(turn45R))
+        {
+            knightAnimator.SetTrigger(sideR);
+        }
+        if (Input.GetKeyDown(attack))
+        {
+            knightAnimator.SetTrigger(hit);
+        }
+        if (Input.GetKeyDown(die))
+        {
+            knightAnimator.SetTrigger(attacked);
+        }
+        if (Input.GetKeyDown(idle))
+        {
+            knightAnimator.SetTrigger(idling);
+        }
+    }
+
+}
---
+++ assignment/RandomChess/Assets/_script/PawnAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnAnimation : MonoBehaviour
+{
+    public Animator pawnAnimator;
+    public KeyCode testStraight;
+    public KeyCode testAttackL;
+    public KeyCode testAttackR;
+    public KeyCode testAttacked;
+    const string moveStraight = "Straight";
+    const string moveDiagonalL = "AttackL";
+    const string moveDiagonalR = "AttackR";
+    const string attacked = "Die";
+
+    void Start()
+    {
+        //cameraAnimator.GetComponent<cameraAnimator>(); -> dont need this because we have public Animator
+    }
+
+        }
+        if (Input.GetKeyDown(testAttacked))
+        {
+            pawnAnimator.SetTrigger(attacked);
+        }
+    }
+}
