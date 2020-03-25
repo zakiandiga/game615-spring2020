83fffd275053b05c5b2d415c981b55435d9968d5
Zak <rusty_clarinet@yahoo.com>
2020-03-25 12:55:52 -0400

    W10 Progress (buggy)

---
+++ assignment/RandomChess/Assets/_script/AI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    string ai_player = "Black";
+    List<Transform> aiPieces = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject t in GameObject.FindGameObjectsWithTag("chessPieces"))
+        {
+            if (t.name.Contains(ai_player))
+                aiPieces.Add(t.transform);
+        }
+    }
+
+    void makeRandomMove()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GameState.playersTurn)
+        {
+            makeRandomMove();
+        }
+    }
+}
